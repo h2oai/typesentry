fb951a79d8f7eca735f2a0ddf0db3e4842b9b8d2
Pasha Stetsenko <pasha@h2o.ai>
2017-01-26 16:05:47 -0800

    Export classes U, I and NOT

--- typesentry/__init__.py
+++ typesentry/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # Copyright 2017 H2O.ai; Apache License v2.0;  -*- encoding: utf-8 -*-
 
-from .checks import MagicType, checker_for_type
+from .checks import MagicType, checker_for_type, U, I, NOT
 from .config import Config
 from .__version__ import version as __version__
 
-__all__ = ("checker_for_type", "Config", "MagicType", "__version__")
+__all__ = ("checker_for_type", "Config", "MagicType", "U", "I", "NOT",
+           "__version__")
