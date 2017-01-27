429eec53cba7885c81ebb9c37ab327caad91a369
Pasha Stetsenko <pasha@h2o.ai>
2017-01-26 16:06:54 -0800

    Add more tests for is_type

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 from __future__ import print_function
+import pytest
 
 import sys
 sys.path.insert(0, "..")
 
 import typesentry  # noqa
+from typesentry import U, I, NOT  # noqa
 
 _tc = typesentry.Config()
 typed = _tc.typed
@@ -13,4 +15,12 @@ is_type = _tc.is_type
 TypeError = _tc.TypeError
 ValueError = _tc.ValueError
 
-__all__ = ("is_type", "typed", "TypeError", "ValueError")
+
+#------ pytest fixtures --------------------------------------------------------
+
+py3only = pytest.mark.skipif(sys.version_info < (3, 5),
+                             reason="at least mymodule-1.1 required")
+
+
+__all__ = ("is_type", "typed", "py3only", "TypeError", "ValueError",
+           "U", "I", "NOT")
--- tests/test_is_type.py
+++ tests/test_is_type.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
-from tests import is_type
+from tests import is_type, py3only, U
 
 
 def test_literals():
@@ -11,7 +11,9 @@ def test_literals():
     assert is_type("foo", "foo", "bar")
     assert is_type(u"foo", "foo", "bar")
     assert is_type("foo", u"foo", u"bar")
+    assert is_type("I", *list("ABCDEFGHIJKL"))
     assert is_type(None, None)
+    assert is_type(None, int, str, None)
     assert not is_type(1, 2)
     assert not is_type(False, None)
 
@@ -20,9 +22,11 @@ def test_primitives():
     assert is_type(False, bool)
     assert is_type(True, bool)
     assert is_type(1, int)
-    assert is_type(1234567891011122345, int)
+    assert is_type(123456789101112123456789, int)
     assert is_type(1, float)
     assert is_type(1.1, float)
+    assert is_type(float("nan"), float)
+    assert is_type(float("inf"), float)
     assert is_type("hello", str)
     assert is_type(u"hello", str)
     assert is_type(b"hello", str)
@@ -36,6 +40,8 @@ def test_primitives():
 
 
 def test_lists():
+    assert is_type([], [int])
+    assert is_type([], [[[list]]])
     assert is_type([1, 2], list)
     assert is_type([1, 2, 3], [int])
     assert is_type([1, 2, 3], [float])
@@ -43,6 +49,7 @@ def test_lists():
     assert is_type([1, 2, "hello", None], [int, str, None])
     assert is_type([[1]], [list])
     assert is_type([[1], [2, 3], [0]], [[int]])
+    assert is_type([[2.0, 3.1, 0], [2, 4.4, 1.1], [-1, 0]], [[int, float]])
     assert not is_type([1, 2, "hello", None], [str, None])
     assert not is_type([[1]], [int])
 
@@ -51,8 +58,69 @@ def test_sets():
     assert is_type({1}, set)
     assert is_type(set(), set)  # literal {} is a dict!
     assert is_type(set(), {int})
-    assert is_type({1, 2, 3}, {int})
+    assert is_type({1, 2, 1, 1, 3}, {int})
     assert is_type({1, 2, 3}, {float})
     assert is_type({1, 2, 3}, {float, str, None})
     assert is_type({1, 2, 3.5, None, "Aye"}, {float, str, None})
     assert not is_type({1, 2, 3.5, None, False}, {float, str, None})
+
+
+def test_dicts():
+    assert is_type({"foo": 1, "bar": 2}, {str: int})
+    assert is_type({"foo": 3, "bar": [5], "baz": None},
+                   {str: U(None, int, [int])})
+    assert is_type({"foo": 1, "bar": 2},
+                   {"foo": int, "bar": U(int, float, None), "baz": bool})
+    assert is_type({}, {"spam": int, "egg": int})
+    assert is_type({"spam": 10}, {"spam": int, "egg": int})
+    assert is_type({"egg": 1}, {"spam": int, "egg": int})
+    assert is_type({"egg": 1, "spam": 10}, {"spam": int, "egg": int})
+    # assert is_type({"egg": 1, "spam": 10}, Dict(egg=int, spam=int))
+    # assert is_type({"egg": 1, "spam": 10},
+    #                Dict(egg=int, spam=int, ham=U(int, None)))
+    assert not is_type({"foo": 1}, {str: str})
+    assert not is_type({"foo": 1, "bar": 2}, {"foo": int})
+    assert not is_type({"foo": 1, "bar": 2}, {"foo": int, str: str})
+
+
+@py3only
+def test_Any():
+    from typing import Any
+    assert is_type(0, Any)
+    assert is_type(None, Any)
+    assert is_type(False, Any)
+    assert is_type("", Any)
+    assert is_type(lambda x: x + 1, Any)
+    assert is_type([1, 3, 5], Any)
+
+
+@py3only
+def test_Union():
+    from typing import Union, List
+    assert is_type(1, Union[str, int])
+    assert is_type("1", Union[str, int])
+    assert is_type([1, 0, "hi"], List[Union[str, int]])
+    assert not is_type(Union, Union[str, int])
+
+    # In Python, bool is a subclass of int, and thus Union[bool, int] = int.
+    # However we make a distinction between booleans and integers, hence
+    # is_type(True, int) is False.
+    assert not is_type(True, Union[bool, int])
+
+
+@py3only
+def test_List():
+    from typing import List, Any
+    assert is_type([], List)
+    assert is_type([], List[int])
+    assert is_type([], List[str])
+    assert is_type([1, 4, -1], List[int])
+    assert is_type([1, 4, -1], List[float])
+    assert is_type(["sam", "ham", "hum", "bum"], List[str])
+    assert is_type([1, False, "monkey"], List)
+    assert is_type([1, False, "monkey"], List[Any])
+
+
+# @py3only
+# def test_Dict():
+#     from typing import Dict
